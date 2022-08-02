1e06f54a7a11b4a743907344350a1d24ef62196e
Akio <cristian.akio1102@gmail.com>
2022-08-01 21:27:01 -0500

    Redeclaracion y Reaccionacion con var,let,conts

---
+++ src/re.js
@@ -0,0 +1,32 @@
+// VAR
+var firtsName; //Undefined
+firtsName = "Akio"; //Asignamos
+console.log(firtsName); //Invocamos
+
+var lastName = "Diaz"; //Declara / Asignar
+lastName = "Aza"; // Reasignar
+console.log(lastName);
+
+var secondName = "Akio"; //Declara / Asignar
+var secondName = "Cristian"; // Reasignar
+console.log(secondName);
+
+// LET
+let fruit = "Apple"; // Declara y Asignar
+fruit = "Kiwi"; // Reasignar
+console.log(fruit);
+
+// let fruit = "Banana"; No puede redeclarar
+console.log(fruit);
+
+//CONTS
+const animal = "Perro"; // Declara y Asignar
+// animal = "Gato";  Reasignar ni tampoco Redeclarar
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("carrito");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
