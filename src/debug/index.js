4b118fee836d19f5f4d2eb8ac39bd63598569947
Akio <cristian.akio1102@gmail.com>
2022-08-06 19:27:48 -0500

    The Fin

---
+++ src/debug/index.js
@@ -0,0 +1,9 @@
+var a = hello;
+
+function hello() {
+  let b = "hello Word";
+  const c = "hello Word";
+  debugger;
+}
+
+hello();
---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+// var nameOfDog //Undefined por que se declara más no se inicializa
+
+// console.log(nameOfDog);
+
+// var nameOfDog = "Tony";
+// console.log(nameOfDog);
+
+var dog; //undefined
+nameOfDog();
+
+function nameOfDog() {
+  console.log(`El mejor perrito es ${dog}`);
+}
+
+var dog = "lucky";
